47b0a20dd1653791752b7b62699bf93602c5e86d
Niyaz Munna <niyaz.munna09@gmail.com.com>
2021-02-22 09:15:39 +0530

    finel submit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            console.log('jello');
+            $("nav").css({ "background-color": "white" });
+        }
+        else {
+            $("nav").css({ "background-color": "transparent" });
+            console.log($(window).scrollTop());
+        }
+
+    })
+})
+
+
+
+
+
+
 let opacity = [];
 for (var i = 1; i <= 8; i++) {
     opacity.push(document.getElementById(`op-${i}`));
@@ -5,7 +24,7 @@ for (var i = 1; i <= 8; i++) {
 const hoverAnimation = (element) => {
     opacity.map((a) => {
         if (a.id != element.id) {
-            a.style.opacity = 0.5
+            a.style.opacity = 0.7
         }
         else a.style.opacity = 1
     });
@@ -15,3 +34,22 @@ const backToNomral = (element) => {
         a.style.opacity = 1
     });
 };
+
+
+// let opacityb = [];
+// for (var i = 1; i <= 4; i++) {
+//     opacityb.push(document.getElementById(`opp-${i}`));
+// }
+// const hoverAnimation = (element) => {
+//     opacityb.map((b) => {
+//         if (b.id != element.id) {
+//             b.style.opacityb = 0.7
+//         }
+//         else b.style.opacityb = 1
+//     });
+// };
+// const backToNomral = (element) => {
+//     opacity.map((b) => {
+//         b.style.opacityb = 1
+//     });
+// };
